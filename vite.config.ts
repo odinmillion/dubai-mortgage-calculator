b1f354e805b1cac56044b0b04409c0069b2c1208
Evgeniy Yurev <evgenijurev@Evgenijs-MacBook-Pro.local>
2025-06-03 00:41:56 +0400

    add CNAME

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), {
+    name: 'copy-cname',
+    closeBundle() {
+      fs.copyFileSync('CNAME', resolve(__dirname, 'dist/CNAME'))
+    }
+  }],
   base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
